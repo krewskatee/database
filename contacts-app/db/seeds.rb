8540eda7b192d1ca55a5bb51872d94cf3064f493
krewskatee <lchira99@gmail.com>
2017-08-29 18:51:45 -0500

    added faker gem

--- contacts-app/db/seeds.rb
+++ contacts-app/db/seeds.rb
@@ -1,23 +1,12 @@
-contact = Contact.new(
-                    first_name: "Luke",
-                    last_name: "Chirayil",
-                    email: "luke@gmail.com",
-                    phone_number: "6305550990"
-                    )
-contact.save
 
-contact = Contact.new(
-                    first_name: "Jane",
-                    last_name: "Brown",
-                    email: "jane@gmail.com",
-                    phone_number: "7125550650"
-                    )
-contact.save
-
-contact = Contact.new(
-                    first_name: "Blake",
-                    last_name: "Torphy",
-                    email: "blake@gmail.com",
-                    phone_number: "8475558789"
-                    )
-contact.save
+100.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  contact = Contact.new(
+                      first_name: first_name,
+                      last_name: last_name,
+                      email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
+                      phone_number: Faker::PhoneNumber.phone_number
+                      )
+  contact.save
+end
--- cookbook-app/app/controllers/recipes_controller.rb
+++ cookbook-app/app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def one_recipe_action
     @recipes = Recipe.all
-    render 'one_recipe_page.html.erb'
+    render 'all_recipe_page.html.erb'
   end
 end
