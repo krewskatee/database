405a4bbb19e3e22ec06b1828efbbb7bb64e7f5dc
krewskatee <lchira99@gmail.com>
2017-08-30 21:22:45 -0500

    added forms

--- contacts-app/app/controllers/contacts_controller.rb
+++ contacts-app/app/controllers/contacts_controller.rb
@@ -1,8 +1,13 @@
 class ContactsController < ApplicationController
+
   def contact_action
-
-    @contacts = Contact.all
-
     render 'contacts.html.erb'
+  end
+
+  def send_contacts
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @email = params[:email]
+    @phone_number = params[:phone_number]
   end
 end
--- contacts-app/config/routes.rb
+++ contacts-app/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/contacts' => 'contacts#contact_action'
+  post '/send_contacts' => 'contacts#send_contacts'
 end
